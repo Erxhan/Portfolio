a35a819c858f0c466bb7302cf217ec2e871e60da
erdzanselmani <31615232+erdzanselmani@users.noreply.github.com>
2018-02-05 13:54:58 +0100

    Little tweaks

--- src/components/pages/Page1.js
+++ src/components/pages/Page1.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './Page1.css';
 
 class Page1 extends React.Component {
-  showText = () => {
-    console.log(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      textShow: false
+    }
+  }
+  showText= () => {
+    this.setState({
+      textShow: true
+    })
   }
   render() {
     return (
@@ -11,8 +19,8 @@ class Page1 extends React.Component {
         <h1>Erxhan Selmani</h1>
         <h2>Web Application Developer</h2>
         <h3>Front End, Back End, UI & UX</h3>
-        <button id="more" onClick={this.showText}>Plus</button>
-        <p id="texte">
+        { this.state.textShow 
+          ? <p id="texte">
           Passioné de Technologie et de Design, j'ai décidé de combiné les deux et d'en faire ma vocation.
           <br />
           C'est ainsi qu'aujourd'hui, j'ai mis un pied dans le monde du développemet Web et Mobile en tant que
@@ -21,7 +29,8 @@ class Page1 extends React.Component {
           Toute la stack du développement suscite pour moi un réel intérêt, que ce soit de la logique métier en back-end, en passant
           par la mise en place de SPA en front-end jusqu'aux travaux de design et de conception d'idées. J'ai
           néanmoins un penchant pour tout ce qui a attrait au visuel.
-          </p>
+          </p> 
+          : <button id="more" onClick={this.showText}>Plus</button> }
       </div>
     );
   };
