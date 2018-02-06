00ed949f0191e37dd81da359fb33ade6858d4241
erdzanselmani <31615232+erdzanselmani@users.noreply.github.com>
2018-02-06 16:43:07 +0100

    Style changes

--- src/components/pages/Page1.js
+++ src/components/pages/Page1.js
@@ -20,7 +20,8 @@ class Page1 extends React.Component {
         <h2>Web Application Developer</h2>
         <h3>Front End, Back End, UI & UX</h3>
         { this.state.textShow 
-          ? <p id="texte">
+          ? <div id="texte">
+          <div className="texteInside">
           Passioné de Technologie et de Design, j'ai décidé de combiné les deux et d'en faire ma vocation.
           <br />
           C'est ainsi qu'aujourd'hui, je me retrouve dans le monde du développemet Web et Mobile en tant que
@@ -29,7 +30,9 @@ class Page1 extends React.Component {
           Toute la stack du développement suscite pour moi un réel intérêt, que ce soit de la logique métier en back-end, en passant
           par la mise en place de SPA en front-end jusqu'aux travaux de design et de conception d'idées. J'ai
           néanmoins un penchant pour tout ce qui a attrait au visuel.
-          </p> 
+          </div>
+          <div className="exitModal" onClick={() => this.setState({textShow: false})}>X</div>
+          </div> 
           : <button id="more" onClick={this.showText}>Plus</button> }
       </div>
     );
