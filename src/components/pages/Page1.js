6005d1cff4e61b7da9ebbfb942340de0c5d717e4
erdzanselmani <31615232+erdzanselmani@users.noreply.github.com>
2018-02-06 16:50:28 +0100

    Style changes

--- src/components/pages/Page1.js
+++ src/components/pages/Page1.js
@@ -22,16 +22,16 @@ class Page1 extends React.Component {
         { this.state.textShow 
           ? <div id="texte">
           <div className="texteInside">
-          Passioné de Technologie et de Design, j'ai décidé de combiné les deux et d'en faire ma vocation.
-          <br />
-          C'est ainsi qu'aujourd'hui, je me retrouve dans le monde du développemet Web et Mobile en tant que
-          développeur Junior.
-          <br />
-          Toute la stack du développement suscite pour moi un réel intérêt, que ce soit de la logique métier en back-end, en passant
-          par la mise en place de SPA en front-end jusqu'aux travaux de design et de conception d'idées. J'ai
-          néanmoins un penchant pour tout ce qui a attrait au visuel.
+            Passioné de Technologie et de Design, j'ai décidé de combiné les deux et d'en faire ma vocation.
+            <br />
+            C'est ainsi qu'aujourd'hui, je me retrouve dans le monde du développemet Web et Mobile en tant que
+            développeur Junior.
+            <br />
+            Toute la stack du développement suscite pour moi un réel intérêt, que ce soit de la logique métier en back-end, en passant
+            par la mise en place de SPA en front-end jusqu'aux travaux de design et de conception d'idées. J'ai
+            néanmoins un penchant pour tout ce qui a attrait au visuel.
           </div>
-          <div className="exitModal" onClick={() => this.setState({textShow: false})}>X</div>
+          <div className="exitModal" onClick={() => this.setState({textShow: false})}>Fermer</div>
           </div> 
           : <button id="more" onClick={this.showText}>Plus</button> }
       </div>
