62c946ca98635e9ed0247b1b05030601093e52ee
erdzanselmani <31615232+erdzanselmani@users.noreply.github.com>
2018-02-09 14:26:00 +0100

    Conditional Rendering

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,14 +8,42 @@ import Page4 from './pages/Page4';
 class Content extends React.Component {
   render()  {
     console.log(this.props.currentPage);
-    return (
-      <div className="Content">
-        <Page1 className={this.props.currentPage === 'page1' ? "show" : "hide"}></Page1>
-        <Page2 className={this.props.currentPage === 'page2' ? "show" : "hide"}></Page2>
-        <Page3 className={this.props.currentPage === 'page3' ? "show" : "hide"}></Page3>
-        <Page4 className={this.props.currentPage === 'page4' ? "show" : "hide"}></Page4>
-      </div>
-    )
+    switch (this.props.currentPage) {
+      case 'page1':
+        return (
+          <div className="Content">
+            <Page1 className="show"></Page1>
+          </div>
+        )
+      case 'page2':
+        return (
+          <div className="Content">
+            <Page2 className="show"></Page2>
+          </div>
+        )
+      case 'page3':
+        return (
+          <div className="Content">
+            <Page3 className="show"></Page3>
+          </div>
+        )
+      case 'page4':
+        return (
+          <div className="Content">
+            <Page4 className="show"></Page4>
+          </div>
+        )
+      default:
+        break;
+    }
+    // return (
+    //   <div className="Content">
+    //     <Page1 className={this.props.currentPage === 'page1' ? "show" : "hide"}></Page1>
+    //     <Page2 className={this.props.currentPage === 'page2' ? "show" : "hide"}></Page2>
+    //     <Page3 className={this.props.currentPage === 'page3' ? "show" : "hide"}></Page3>
+    //     <Page4 className={this.props.currentPage === 'page4' ? "show" : "hide"}></Page4>
+    //   </div>
+    // )
   };
 };
 
