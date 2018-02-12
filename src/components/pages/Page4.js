51dec3158ecd44d8f0dfd62c2a875cb7d03683ad
erdzanselmani <31615232+erdzanselmani@users.noreply.github.com>
2018-02-12 16:22:48 +0100

    Contact Page

--- src/components/pages/Page4.js
+++ src/components/pages/Page4.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './Page4.css';
+import Email from '../../img/email.svg';
+import Github from '../../img/github.svg';
+import Iphone from '../../img/iphone.svg';
+import Linkedin from '../../img/linkedin.svg';
 
 class Page4 extends React.Component {
   render() {
+    let logoStyle = {
+      width: '64px',
+      height: '64px',
+      marginBottom: '1em'
+    };
+    
     return (
       <div className={`page ${this.props.className}`} id="page4">
-        <h1>Under Construction <span role="img" aria-label="construction">🚧</span></h1>
+        <div className="card card-1">  
+          <img style={logoStyle} src={Email}></img>
+          <a className="linkStyle" href="mailto:erxhanselmani@icloud.com">Email Me</a> 
+        </div>
+        <div className="card card-2">
+          <img style={logoStyle} src={Github}></img>
+          <a className="linkStyle" target="_blank" href="https://github.com/Erxhan">Github</a>
+        </div>
+        <div className="card card-3">
+          <img style={logoStyle} src={Iphone}></img>
+          <a className="linkStyle" href="tel:+32484621746">Call Me</a>
+        </div>
+        <div className="card card-4">
+          <img style={logoStyle} src={Linkedin}></img>
+          <a className="linkStyle" target="_blank" href="https://www.linkedin.com/in/erdzan-selmani/">Visit Me</a>
+        </div>
       </div>
     );
   };
