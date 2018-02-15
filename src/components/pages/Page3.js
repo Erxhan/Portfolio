ff406c6f21cd026a3d4ab18c5d8f413e7dcd5d5b
erdzanselmani <31615232+erdzanselmani@users.noreply.github.com>
2018-02-15 14:51:09 +0100

    Technology Logos

--- src/components/pages/Page3.js
+++ src/components/pages/Page3.js
@@ -1,11 +1,114 @@
 import React from 'react';
 import './Page3.css';
 
+import AdobeXD from '../../img/adobe-xd.svg';
+import Bootstrap4 from '../../img/bootstrap4.svg';
+import Css3 from '../../img/css3.svg';
+import Electron from '../../img/electron.svg';
+import GithubLogo from '../../img/github-logo.svg';
+import Graphql from '../../img/graphql.svg';
+import Heroku from '../../img/heroku.svg';
+import Html5 from '../../img/html5.svg';
+import Javascript from '../../img/javascript.svg';
+import Json from '../../img/json.svg';
+import Laravel from '../../img/laravel.svg';
+import Mongodb from '../../img/mongodb.svg';
+import Mysql from '../../img/mysql.svg';
+import Nodejs from '../../img/nodejs.svg';
+import Photoshop from '../../img/photoshop.svg';
+import Php from '../../img/php.svg';
+import ReactLogo from '../../img/react.svg';
+import Redux from '../../img/redux.svg';
+import Sass from '../../img/sass.svg';
+import Swift from '../../img/swift.svg';
+import Symfony from '../../img/symfony.svg';
+
 class Page3 extends React.Component {
   render() {
     return (
       <div className={`page ${this.props.className}`} id="page3">
-        <h1>Under Construction <span role="img" aria-label="construction">🚧</span></h1>
+        <div className="container">
+          <figure>
+          <img src={Javascript}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Html5}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Css3}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Sass}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Bootstrap4}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={ReactLogo}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Redux}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Graphql}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Json}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Nodejs}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Electron}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Mongodb}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Mysql}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Php}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Laravel}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Symfony}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Swift}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Heroku}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={AdobeXD}></img>
+          <span></span>
+        </figure>
+        <figure>
+          <img src={Photoshop}></img>
+          <span></span>
+        </figure>
+        </div>
       </div>
     );
   };
